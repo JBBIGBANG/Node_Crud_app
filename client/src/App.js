e29cac69d6c2027769d5cc27a4da65ca07c7b4bd
Joy Sarkar <sjoy.cse3.bu@gmail.com>
2022-02-07 23:57:15 +0600

    add product form and router work done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import './App.css';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import AddProduct from './components/AddProduct';
 function App() {
   return (
    <>
-    <Navigation />
-    <Home />
+    <Router>
+        <Navigation />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/addProduct" element={<AddProduct />} />   
+        </Routes>
+    </Router>
    </>
+    
   );
 }
 
